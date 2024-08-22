9b0551cd3d1c08d2f1899b021306ce0034febf6b
Zhaqian Rouf Alfauzi <zhaqianroufa@gmail.com>
2024-08-22 13:40:32 +0700

    all

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function ageVerified() {
+  alert("Thank you! You may enter the site.");
+  window.location.href = "https://unionlabs.id/";
+}
+
+function underage() {
+  alert("You're Under Age");
+}
